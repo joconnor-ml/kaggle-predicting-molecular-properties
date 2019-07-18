c2799798d4241d30198540b4abf4c5df74d243d9
joe <joconnor@quant-co.com>
2019-07-18 13:58:57 +0300

    Process test data

--- process_data.sh
+++ process_data.sh
@@ -1,1 +1,3 @@
-python process_data.py sample_data/csv/train.csv sample_data/structures/ sample_data/graphs
+python process_data.py sample_data/csv/train.csv sample_data/structures/ sample_data/graphs;
+python process_data.py sample_data/csv/test.csv sample_data/structures/ sample_data/test/graphs
+

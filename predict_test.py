Fix normalisation

predict_test.py
@@ -21,9 +21,6 @@ if __name__ == "__main__":
     dim = 64
 
     test_dataset = ChampsTestDataset("./data/")
-    # Normalize targets to mean = 0 and std = 1.
-    mean = 15.9187472
-    std = 34.93184728
 
     # Split datasets.
     test_loader = DataLoader(
@@ -36,6 +33,14 @@ if __name__ == "__main__":
     checkpoint = torch.load(args.checkpoint_file, map_location=device)
     model.load_state_dict(checkpoint)
 
+    # Normalize targets to mean = 0 and std = 1.
+    mean = torch.from_numpy(
+        np.array([14.4633,  0.4420, -0.0663, -0.8348,  0.0800,  1.1960,  0.6049,  0.0354]),
+    ).to(device)
+    std = torch.from_numpy(
+        np.array([34.8631,  4.6798,  2.2415,  3.0292,  0.7675,  2.4574,  2.0641,  0.3092])
+    ).to(device)
+
     def test(loader):
         model.eval()
         error = 0
@@ -43,11 +48,13 @@ if __name__ == "__main__":
         preds = []
         for data in loader:
             data = data.to(device)
-            preds.append(model(data).detach().cpu().numpy())
+            p = model(data) * std + mean
+            p = torch.gather(p, 1, data.target_class.view(-1, 1)).squeeze(-1)
+            preds.append(p.detach().cpu().numpy())
 
         return np.concatenate(preds)
 
     preds = test(test_loader)
-    preds = pd.Series(preds) * std + mean
+    preds = pd.Series(preds)
     print(preds.head())
     preds.to_csv("output/preds.test.csv")
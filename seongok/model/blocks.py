aa7b633694e2f4d46bb8432189d03209f5f0ffdc
joe <joconnor@quant-co.com>
2019-07-21 14:22:55 +0300

    fix tiling for cartesian product

--- seongok/model/blocks.py
+++ seongok/model/blocks.py
@@ -305,8 +305,8 @@ def readout_edgewise(X, latent_size):
     # edge_representation.shape = [num_atoms, num_atoms, hidden_dim]
 
     # now let's combine into edge representation
-    atom1 = tf.tile(atom_representation, num_atoms)
-    atom2 = tf.reshape(tf.tile(tf.expand_dims(atom_representation, axis=-1), num_atoms), shape=[-1])
+    atom1 = tf.tile(atom_representation, [1, num_atoms, 1])
+    atom2 = tf.reshape(tf.tile(tf.expand_dims(atom_representation, axis=-1), [1,1,1, num_atoms]), shape=[-1])
 
     edge_representation = tf.concat([atom1, atom2], 1)
 

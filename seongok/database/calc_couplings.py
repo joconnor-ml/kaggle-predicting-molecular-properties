1b6e2cdf014aae120694b066137613a502f29fc3
joe <joconnor@quant-co.com>
2019-07-21 14:12:44 +0300

    Multiprocess

--- seongok/database/calc_couplings.py
+++ seongok/database/calc_couplings.py
@@ -1,5 +1,7 @@
 import numpy as np
 import pandas as pd
+import multiprocessing
+
 
 train = pd.read_csv("../../data/csv/train.csv")
 print(train["atom_index_1"].max())
@@ -7,22 +9,22 @@ molecule_names = train["molecule_name"].unique()
 
 grps = train.groupby("molecule_name")
 
-masks = []
-targets = []
-for i, mol in molecule_names:
-    if i % 10000 == 0:
-        print(i)
+
+def process(mol):
     df = grps.get_group(mol)
     mask = np.zeros((50,50), dtype=np.uint8)
     mask[df["atom_index_0"].values, df["atom_index_1"].values] = 1
-    masks.append(mask)
 
     target = np.zeros((50,50), dtype=np.float32)
     target[df["atom_index_0"].values, df["atom_index_1"].values] = df["scalar_coupling_constant"]
-    targets.append(target)
+    return target, mask
 
-masks = np.asarray(masks)
-targets = np.asarray(targets)
+
+pool = multiprocessing.Pool(multiprocessing.cpu_count())
+results = pool.map(process, molecule_names)
+
+masks = np.asarray([mask for target, mask in results])
+targets = np.asarray([target for target, mask in results])
 
 np.save('./CHAMPS/scalar_coupling.npy', targets)
 np.save('./CHAMPS/scalar_coupling.mask.npy', masks)

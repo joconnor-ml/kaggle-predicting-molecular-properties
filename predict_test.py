fa0ae9ca528dba09d0227e5dd56f1f148803aea5
joe <joconnor@quant-co.com>
2019-08-02 15:13:03 +0100

    Refactor test script

--- predict_test.py
+++ predict_test.py
@@ -1,6 +1,6 @@
 import sys
 sys.path.append("..")
-from champs.datasets import ChampsTestDataset
+from champs.datasets import ChampsTestDatasetTargetSubset
 from champs.models import Net
 from torch_geometric.data import DataLoader
 import torch.nn.functional as F
@@ -9,20 +9,11 @@ import torch
 import pandas as pd
 import numpy as np
 
-if __name__ == "__main__":
 
-    import argparse
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument("checkpoint_file", type=str)
-    parser.add_argument("output_file", type=str)
-    parser.add_argument("--device", "-d", type=str)
-    args = parser.parse_args()
-
-    target = 0
+def main(target_classes, device, checkpoint_file, output_file):
     dim = 64
 
-    test_dataset = ChampsTestDataset("./data/")
+    test_dataset = ChampsTestDatasetTargetSubset("./data/", targets=target_classes)
 
     # Split datasets.
     test_loader = DataLoader(
@@ -31,9 +22,9 @@ if __name__ == "__main__":
         pin_memory=True
     )
 
-    device = torch.device(args.device)
+    device = torch.device()
     model = Net(test_dataset.num_features, test_dataset[0].edge_attr.shape[-1], dim).to(device)
-    checkpoint = torch.load(args.checkpoint_file, map_location=device)
+    checkpoint = torch.load(checkpoint_file, map_location=device)
     model.load_state_dict(checkpoint)
 
     def test(loader):
@@ -49,4 +40,19 @@ if __name__ == "__main__":
     preds = test(test_loader)
     preds = pd.Series(preds)
     print(preds.head())
-    preds.to_csv(args.output_file)
+    preds.to_csv(output_file)
+
+
+if __name__ == "__main__":
+
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("checkpoint_file", type=str)
+    parser.add_argument("output_file", type=str)
+    parser.add_argument("--target-classes", "-t", nargs="+", type=int, default=None)
+    parser.add_argument("--device", "-d", type=str)
+    args = parser.parse_args()
+
+    main(target_classes=args.target_classes, checkpoint_file=args.checkpoint_file,
+         device=args.device, output_file=args.output_file)

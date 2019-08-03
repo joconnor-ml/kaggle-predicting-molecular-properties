ce919697ad30bd99c622728c281ab6ba50617f16
joe <joconnor@quant-co.com>
2019-08-03 13:21:58 +0100

    debug

--- champs/layers.py
+++ champs/layers.py
@@ -143,7 +143,7 @@ class GATEdgeConv(MessagePassing):
     def message(self, x_j, pseudo):
         # from NNConv:
         weight = self.nn(pseudo).view(-1, self.in_channels, self.out_channels)
-        x_j1 = torch.matmul(x_j.unsqueeze(1), weight).squeeze(1)
+        x_j1 = torch.matmul(x_j.unsqueeze(1), weight)#.squeeze(1)
         print(x_j1.size())
         # now add gated skip connection:
         return self.gated_skip_connection(x_j, x_j1)
